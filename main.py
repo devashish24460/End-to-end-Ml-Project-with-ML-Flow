330e561fe1d90add4eacf3170c06b5d80c7de628
Devashish.M <122509170+devashish24460@users.noreply.github.com>
2025-03-20 16:42:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
